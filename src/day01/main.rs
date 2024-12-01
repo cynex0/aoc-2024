1e079c24240c6605fc51e7367fce46742d34bf0b
Maksims Orlovs <m.orlov10@gmail.com>
2024-12-01 16:36:03 +0100

    Initial solution

--- src/day01/main.rs
+++ src/day01/main.rs
@@ -1,3 +1,21 @@
+fn get_distances(a: Vec<u32>, b: Vec<u32>) -> Vec<u32> {
+    let mut distances = Vec::with_capacity(a.len());
+    let mut a_sorted = a.to_vec();
+    let mut b_sorted = b.to_vec();
+    a_sorted.sort();
+    b_sorted.sort();
+
+    for (a, b) in a_sorted.iter().zip(b_sorted.iter()) {
+        distances.push(a.abs_diff(*b));
+    }
+
+    distances
+}
+
 fn main() {
-    println!("Hello, Advent of Code 2024!");
+    let a = vec![3, 4, 2, 1, 3, 3];
+    let b = vec![4, 3, 5, 3, 9, 3];
+    let distances = get_distances(a, b);
+    let sum: u32 = distances.iter().sum();
+    println!("{}", sum);
 }
