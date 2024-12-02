604e14e34d260892d86135a7d6a9b6b81f22b95e
Maksims Orlovs <m.orlov10@gmail.com>
2024-12-03 00:34:39 +0100

    d02_p2: Complete part 2

--- src/day02/main.rs
+++ src/day02/main.rs
@@ -15,23 +15,44 @@ fn is_safe(a: &[i32]) -> bool {
     return diffs.iter().all(|&x| x > 0) || diffs.iter().all(|&x| x < 0);
 }
 
+fn is_safe_with_damping(a: &[i32]) -> bool {
+    if is_safe(a) {
+        return true;
+    }
+
+    for i in 0..a.len() {
+        let (left, right) = a.split_at(i);
+        let slice = [left, &right[1..]].concat();
+        if is_safe(&slice) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 fn main() {
     let path = Path::new("input");
     let file = File::open(&path).expect("Error opening file");
 
     let lines = BufReader::new(file).lines();
-    let mut counter = 0;
+    let mut p1_counter = 0;
+    let mut p2_counter = 0;
     for line in lines.flatten() {
         let mut arr = vec![];
         for number in line.split_whitespace() {
             arr.push(number.parse().unwrap());
         }
         if is_safe(&arr) {
-            counter += 1;
+            p1_counter += 1;
+        }
+        if is_safe_with_damping(&arr) {
+            p2_counter += 1;
         }
     }
 
-    println!("Part 1 answer: {}", counter);
+    println!("Part 1 answer: {}", p1_counter);
+    println!("Part 2 answer: {}", p2_counter);
 }
 
 #[cfg(test)]
@@ -58,13 +79,60 @@ mod part1_tests {
 
     #[test]
     fn example4() {
-        let a = [8, 6, 4, 4, 1];
+        let a = [1, 3, 2, 4, 5];
         assert_eq!(false, is_safe(&a));
     }
 
     #[test]
     fn example5() {
+        let a = [8, 6, 4, 4, 1];
+        assert_eq!(false, is_safe(&a));
+    }
+
+    #[test]
+    fn example6() {
         let a = [1, 3, 6, 7, 9];
         assert_eq!(true, is_safe(&a));
+    }
+}
+
+#[cfg(test)]
+mod part2_tests {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        let a = [7, 6, 4, 2, 1];
+        assert_eq!(true, is_safe_with_damping(&a));
+    }
+
+    #[test]
+    fn example2() {
+        let a = [1, 2, 7, 8, 9];
+        assert_eq!(false, is_safe_with_damping(&a));
+    }
+
+    #[test]
+    fn example3() {
+        let a = [9, 7, 6, 2, 1];
+        assert_eq!(false, is_safe_with_damping(&a));
+    }
+
+    #[test]
+    fn example4() {
+        let a = [1, 3, 2, 4, 5];
+        assert_eq!(true, is_safe_with_damping(&a));
+    }
+
+    #[test]
+    fn example5() {
+        let a = [8, 6, 4, 4, 1];
+        assert_eq!(true, is_safe_with_damping(&a));
+    }
+
+    #[test]
+    fn example6() {
+        let a = [1, 3, 6, 7, 9];
+        assert_eq!(true, is_safe_with_damping(&a));
     }
 }
