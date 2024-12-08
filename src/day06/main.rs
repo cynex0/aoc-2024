b6e9d9a3f85936f15ea52c5016262e721bb3940f
Maksims Orlovs <m.orlov10@gmail.com>
2024-12-08 13:22:07 +0100

    d06_p1: refactor

--- src/day06/main.rs
+++ src/day06/main.rs
@@ -1,3 +1,6 @@
+use std::collections::HashSet;
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
 enum Direction {
     UP,
     DOWN,
@@ -5,6 +8,7 @@ enum Direction {
     RIGHT,
 }
 
+#[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
 struct Position {
     x: i32,
     y: i32,
@@ -28,59 +32,48 @@ impl Position {
             Direction::UP => self.dir = Direction::RIGHT,
             Direction::DOWN => self.dir = Direction::LEFT,
         }
-        print!("Turned right ");
     }
 
     fn update(&mut self) {
         let (_x, _y) = self.get_dir_vec();
         self.x += _x;
         self.y += _y;
-        print!("Moved by {_x},{_y}");
     }
 }
 
-fn pathfind(map: &mut Vec<Vec<char>>, pos: &mut Position) {
+fn move_guard(pos: &mut Position, map: &Vec<Vec<char>>, rows: usize, cols: usize) -> bool {
+    let (x, y) = (pos.x, pos.y);
+    let (dx, dy) = (pos.get_dir_vec().0, pos.get_dir_vec().1);
+
+    if x > 0 && x < cols as i32 - 1 && y > 0 && y < rows as i32 - 1 {
+        let next = map[(y + dy) as usize][(x + dx) as usize];
+        if next == '#' {
+            pos.turn_right();
+        }
+    }
+
+    pos.update();
+    pos.x >= 0 && pos.x < cols as i32 && pos.y >= 0 && pos.y < rows as i32
+}
+
+fn pathfind(map: &mut Vec<Vec<char>>, pos: &mut Position) -> HashSet<(usize, usize)> {
     let rows = map.len();
     let cols = map[0].len();
 
-    print!("At {},{}: ", pos.x, pos.y);
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+    visited.insert((pos.x as usize, pos.y as usize));
 
-    while pos.x >= 0 && pos.x < cols as i32 && pos.y >= 0 && pos.y < rows as i32 {
-        let (x, y) = (pos.x as usize, pos.y as usize);
-        map[y][x] = 'X';
-
-        match pos.dir {
-            Direction::UP => {
-                if y >= 1 && map[y - 1][x] == '#' {
-                    pos.turn_right();
-                }
-            }
-            Direction::DOWN => {
-                if y < rows - 1 && map[y + 1][x] == '#' {
-                    pos.turn_right();
-                }
-            }
-            Direction::LEFT => {
-                if x >= 1 && map[y][x - 1] == '#' {
-                    pos.turn_right();
-                }
-            }
-            Direction::RIGHT => {
-                if x < cols - 1 && map[y][x + 1] == '#' {
-                    pos.turn_right();
-                }
-            }
+    loop {
+        if !move_guard(pos, map, rows, cols) {
+            break;
         }
-        pos.update();
-        println!();
+        visited.insert((pos.x as usize, pos.y as usize));
     }
+
+    visited
 }
 
-fn main() {
-    let input = include_str!("input");
-    let mut map: Vec<Vec<char>> = input.lines().map(|x| x.chars().collect()).collect();
-    println!("Rows: {}, Cols: {}", map.len(), map[0].len());
-
+fn get_start_pos(map: &Vec<Vec<char>>) -> Position {
     let mut pos = Position {
         x: (0),
         y: (0),
@@ -96,11 +89,17 @@ fn main() {
             }
         }
     }
-    pathfind(&mut map, &mut pos);
-    println!(
-        "Part 1 answer: {}",
-        map.iter().flatten().filter(|&&x| x == 'X').count()
-    );
+
+    pos
+}
+
+fn main() {
+    let input = include_str!("input");
+    let mut map: Vec<Vec<char>> = input.lines().map(|x| x.chars().collect()).collect();
+
+    let start_pos = get_start_pos(&map);
+    let visited = pathfind(&mut map, &mut start_pos.clone());
+    println!("Part 1 answer: {}", visited.iter().count());
 }
 
 #[cfg(test)]
@@ -121,30 +120,8 @@ mod part1_tests {
                    ......#...";
 
         let mut map: Vec<Vec<char>> = str.lines().map(|x| x.chars().collect()).collect();
-        let mut pos = Position {
-            x: (0),
-            y: (0),
-            dir: (Direction::UP),
-        };
-
-        'outer: for (i, row) in map.iter().enumerate() {
-            for (j, c) in row.iter().enumerate() {
-                if *c == '^' {
-                    pos.x = j as i32;
-                    pos.y = i as i32;
-                    break 'outer;
-                }
-            }
-        }
-
-        pathfind(&mut map, &mut pos);
-        for line in map.iter() {
-            for c in line {
-                print!("{c}");
-            }
-            println!();
-        }
-
-        assert_eq!(41, map.iter().flatten().filter(|&&x| x == 'X').count());
+        let mut pos = get_start_pos(&map);
+        let visited = pathfind(&mut map, &mut pos);
+        assert_eq!(41, visited.iter().count());
     }
 }
